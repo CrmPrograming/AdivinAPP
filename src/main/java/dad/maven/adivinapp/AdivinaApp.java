95091518527b92de07b6fa59bd025227f0d9f66a
César Ravelo Martínez <crmprograming@gmail.com>
2020-10-02 17:49:19 +0100

    Terminada la implementación requerida de la práctica

--- src/main/java/dad/maven/adivinapp/AdivinaApp.java
+++ src/main/java/dad/maven/adivinapp/AdivinaApp.java
@@ -1,8 +1,11 @@
 package dad.maven.adivinapp;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -13,9 +16,71 @@ public class AdivinaApp extends Application {
 	
 	private Button btComprobar;
 	private TextField tfNumero;
+	private int numAleatorio;
+	private int intentos;
+	private final int MIN = 0;
+	private final int MAX = 100;
 	
+	private void inicializarPartida() {
+		generarNumero();
+		intentos = 0;
+	}
+	
+	private void generarNumero() {
+		numAleatorio = (int) (Math.random() * (MAX - MIN + 1) + MIN);		
+	}
+	
+	private void mostrarFallo(int modo, int numero) {
+		String mensajeModo = "El número a adivinar es " + ((modo == 1)? "menor" : "mayor") + " que " + numero;
+		Alert alert = new Alert(AlertType.WARNING);
+		alert.setTitle("AdivinaApp");
+		alert.setHeaderText("¡Has fallado!");
+		alert.setContentText(mensajeModo + "\n\nVuelve a intentarlo.");
+
+		alert.showAndWait();
+	}
+	
+	private void mostrarError() {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("AdivinApp");
+		alert.setHeaderText("Error");
+		alert.setContentText("El número introducido no es válido");
+
+		alert.showAndWait();
+	}
+	
+	private void mostrarAcierto() {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("AdivinApp");
+		alert.setHeaderText("¡Has ganado!");
+		alert.setContentText("Sólo has necesitado " + intentos + " intentos.\n\n" +
+							 "Vuelve a jugar y hazlo mejor.");
+
+		alert.showAndWait();
+	}
+	
+	private void onComprobarButtonAction(ActionEvent e) {
+		intentos++;
+		try {
+			int numero = Integer.parseInt(tfNumero.getText());
+			if (numero < numAleatorio)
+				mostrarFallo(0, numero);
+			else if (numero > numAleatorio)
+				mostrarFallo(1, numero);
+			else {
+				mostrarAcierto();
+				inicializarPartida();
+			}
+		} catch (NumberFormatException error) {
+			mostrarError();
+		}
+			
+	}
+			
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		inicializarPartida();
+		
 		tfNumero = new TextField();
 		tfNumero.setPrefColumnCount(3);
 		tfNumero.setPromptText("Indique el número a adivinar");
@@ -28,6 +93,7 @@ public class AdivinaApp extends Application {
 		btComprobar = new Button();
 		btComprobar.setText("Comprobar");
 		btComprobar.setDefaultButton(true);
+		btComprobar.setOnAction(e -> onComprobarButtonAction(e));
 		
 		VBox root = new VBox();
 		root.setSpacing(5);
